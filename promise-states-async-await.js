8cf1d7e2fc9b3798ea5f1c59d8e791f11a8254dd
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 07:35:57 +0200

    states async

---
+++ promise-states-async-await.js
@@ -0,0 +1,30 @@
+const getRandomNumber = (upperLimit) => {
+	const random = Math.random();
+	const randomNumber = Math.floor(random * upperLimit) + 1;
+	return randomNumber;
+};
+
+const getAsynchronousData = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		const rand = getRandomNumber(2);
+		if (rand === 1) {
+			resolve('Promise resolved successfully');
+		} else {
+			reject('Promise rejected with an error');
+		}
+	}, 1000);
+});
+
+async function loadData() {
+	try {
+		const result = await getAsynchronousData;
+		console.log('FULFILLED:', result);
+	} catch (error) {
+		console.error('REJECTED:', error);
+	} finally {
+		console.log('Promise has settled (either fulfilled or rejected).');
+	}
+}
+
+loadData();
+console.log('Promise is in the pending state.');
